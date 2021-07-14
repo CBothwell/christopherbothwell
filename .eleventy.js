06a12d2e378a50d4a8177c10b4356e782e530ea0
Christopher Bothwell <bothwell.christopher@gmail.com>
2021-07-14 09:40:42 -0500

    Updated config to build favicons

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,29 @@ const Image = require('@11ty/eleventy-img');
 const markdownIt = require('markdown-it');
 const markdownItClass = require('@toycode/markdown-it-class');
 
+const sharp = require('sharp');
+
+async function generateFavicon(filename, width, height) {
+      const fileSize = `${width}x${height}`;
+      const fileName = `favicon-${fileSize}.png`; 
+      sharp(filename, {failOnError: true})
+        .resize(width, height)
+        .png()
+        .toFile(`_site/${fileName}`)
+      return `<link rel="icon" type="image/png" href="/${fileName}" sizes="${fileSize}">`
+}
+
+async function faviconShortcode(img) {
+  //const img = src;
+  console.log(`Building new favicon ${img}`);
+  const faviconList = await Promise.all([
+    generateFavicon(img, 16, 16),
+    generateFavicon(img, 32, 32),
+    generateFavicon(img, 96, 96),
+  ]);
+  return faviconList.join('\n');
+}
+
 async function imageShortcode(src, alt, sizes, imageClassNames) {
   let metadata = await Image(src, {
     widths: [300, 600, 700],
@@ -29,13 +52,16 @@ const mapping = {
 };
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addWatchTarget('./_tmp/style.css')
-  eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
+  eleventyConfig.addWatchTarget('./_site/js/index.js');
+
+  eleventyConfig.addWatchTarget('./_site/style.css')
   eleventyConfig.addShortcode('version', function () {
     return now
   })
   eleventyConfig.addPlugin(syntaxHighlight);
+
   eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  eleventyConfig.addLiquidShortcode("favicon", faviconShortcode); 
 
   const md = markdownIt({linkify: true, html: true});
   md.use(markdownItClass, mapping);
